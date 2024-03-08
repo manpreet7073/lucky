d325b01a4c6eb8226b2e717ebd3cc02f0c8e320d
tom <tom@dimoapp.com>
2024-03-08 10:56:47 +0530

    fix-site-map

--- lucky_cleaning/sitemap.js
+++ lucky_cleaning/sitemap.js
@@ -1,43 +1,34 @@
 const { SitemapStream, streamToPromise } = require('sitemap');
-const axios = require('axios');
-const fs = require('fs');
+const { createReadStream, createWriteStream } = require('fs');
 
+// Define the URLs for the sitemap
 const urls = [
   { url: '/', changefreq: 'daily', priority: 1.0 },
-  { url: '/about-us', changefreq: 'weekly', priority: 0.8 }, // Added About page
-  { url: '/services', changefreq: 'weekly', priority: 0.8 }, // Added Services page
-  { url: '/contact-us', changefreq: 'weekly', priority: 0.8 },// Added contact-us page
-  { url: '/404', changefreq: 'weekly', priority: 0.8 }, // Added 404 page
-  // Add more URLs here if needed
+  { url: '/about-us', changefreq: 'weekly', priority: 0.8 },
+  { url: '/services', changefreq: 'weekly', priority: 0.8 },
+  { url: '/contact-us', changefreq: 'weekly', priority: 0.8 },
+  { url: '/404', changefreq: 'weekly', priority: 0.8 },
+  // Add more URLs as needed
 ];
 
-const generateSitemapXml = async () => {
-  const links = [];
-
-  for (const url of urls) {
-    const { data } = await axios.get('https://cleaningwithlucky.com' + url.url); // Fetching complete URLs
-
-    if (data && data.match && data.match(/<title>/)) {
-      links.push({
-        url: url.url,
-        changefreq: url.changefreq,
-        priority: url.priority,
-        lastmod: new Date().toISOString().split('T')[0],
-      });
-    }
-  }
-
+// Function to generate the sitemap XML
+const generateSitemap = async () => {
   const sitemapStream = new SitemapStream({ hostname: 'https://cleaningwithlucky.com' });
 
-  links.forEach(link => {
-    sitemapStream.write(link);
+  // Add URLs to the sitemap
+  urls.forEach(url => {
+    sitemapStream.write(url);
   });
 
   sitemapStream.end();
 
+  // Convert sitemap stream to a string
   const sitemapXML = await streamToPromise(sitemapStream).then(data => data.toString());
 
-  fs.writeFileSync('public/sitemap.xml', sitemapXML);
+  // Write sitemap XML to a file
+  const writeStream = createWriteStream('public/sitemap.xml');
+  writeStream.write(sitemapXML);
+  writeStream.end();
 };
 
-generateSitemapXml();
+generateSitemap();
